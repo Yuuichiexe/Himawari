9af9a83d9878bcc7371faff2f9dbf7d04186bb6e
Comrade X <106185020+comrade09@users.noreply.github.com>
2023-02-04 18:55:22 +0530

    Update chatbot_sql.py
    Signed-off-by: Comrade X  <106185020+comrade09@users.noreply.github.com>

--- Himawari/modules/sql/chatbot_sql.py
+++ Himawari/modules/sql/chatbot_sql.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2022 Arsh
+Copyright (c) 2022 Aʙɪsʜɴᴏɪ
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -23,43 +23,41 @@ SOFTWARE.
 """
 
 import threading
+
 from sqlalchemy import Column, String
+
 from Himawari.modules.sql import BASE, SESSION
-class KukiChats(BASE):
-    __tablename__ = "kuki_chats"
+
+
+class FallenChats(BASE):
+    __tablename__ = "fallen_chats"
     chat_id = Column(String(14), primary_key=True)
 
     def __init__(self, chat_id):
         self.chat_id = chat_id
 
-KukiChats.__table__.create(checkfirst=True)
+FallenChats.__table__.create(checkfirst=True)
 INSERTION_LOCK = threading.RLock()
 
 
-def is_kuki(chat_id):
+def is_fallen(chat_id):
     try:
-        chat = SESSION.query(KukiChats).get(str(chat_id))
+        chat = SESSION.query(FallenChats).get(str(chat_id))
         return bool(chat)
     finally:
         SESSION.close()
 
-def set_kuki(chat_id):
+def set_fallen(chat_id):
     with INSERTION_LOCK:
-        kukichat = SESSION.query(KukiChats).get(str(chat_id))
-        if not kukichat:
-            kukichat = KukiChats(str(chat_id))
-        SESSION.add(kukichat)
+        fallenchat = SESSION.query(FallenChats).get(str(chat_id))
+        if not fallenchat:
+            fallenchat = FallenChats(str(chat_id))
+        SESSION.add(fallenchat)
         SESSION.commit()
 
-def rem_kuki(chat_id):
+def rem_fallen(chat_id):
     with INSERTION_LOCK:
-        if kukichat := SESSION.query(KukiChats).get(str(chat_id)):
-            SESSION.delete(kukichat)
+        fallenchat = SESSION.query(FallenChats).get(str(chat_id))
+        if fallenchat:
+            SESSION.delete(fallenchat)
         SESSION.commit()
-
-
-def get_all_kuki_chats():
-    try:
-        return SESSION.query(KukiChats.chat_id).all()
-    finally:
-        SESSION.close()
