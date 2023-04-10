1ccce5805ef823cb08d091c2725374b5e614aaaf
Comrade X <106185020+comrade09@users.noreply.github.com>
2023-04-10 21:32:40 +0530

    Update welcome.py
    Signed-off-by: Comrade X  <106185020+comrade09@users.noreply.github.com>

--- Himawari/modules/welcome.py
+++ Himawari/modules/welcome.py
@@ -7,7 +7,7 @@ from contextlib import suppress
 
 import Himawari.modules.sql.welcome_sql as sql
 import Himawari
-from Mizuharabot import (
+from Himawari import (
     DEV_USERS,
     LOGGER,
     OWNER_ID,
@@ -19,18 +19,18 @@ from Mizuharabot import (
     dispatcher,
     JOIN_LOGGER
 )
-from Mizuharabot.modules.helper_funcs.chat_status import (
+from Himawari.modules.helper_funcs.chat_status import (
     is_user_ban_protected,
     user_admin,
 )
-from Mizuharabot.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from Mizuharabot.modules.helper_funcs.msg_types import get_welcome_type
-from Mizuharabot.modules.helper_funcs.string_handling import (
+from Himawari.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from Himawari.modules.helper_funcs.msg_types import get_welcome_type
+from Himawari.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
     markdown_parser,
 )
-from Mizuharabot.modules.log_channel import loggable
-from Mizuharabot.modules.sql.global_bans_sql import is_user_gbanned
+from Himawari.modules.log_channel import loggable
+from Himawari.modules.sql.global_bans_sql import is_user_gbanned
 from telegram import (
     ChatPermissions,
     InlineKeyboardButton,
