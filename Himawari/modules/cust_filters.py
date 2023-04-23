2d2fcf2999adf502d7fc7e95bd763727afd2cb4a
Comrade X <106185020+comrade09@users.noreply.github.com>
2023-04-23 19:24:09 +0530

    Update cust_filters.py
    Signed-off-by: Comrade X  <106185020+comrade09@users.noreply.github.com>

--- Himawari/modules/cust_filters.py
+++ Himawari/modules/cust_filters.py
@@ -1,37 +1,39 @@
-import random
 import re
+import random
 from html import escape
 
 import telegram
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
+from telegram import ParseMode, InlineKeyboardMarkup, Message, InlineKeyboardButton
 from telegram.error import BadRequest
 from telegram.ext import (
-    CallbackQueryHandler,
     CommandHandler,
-    DispatcherHandlerStop,
-    Filters,
     MessageHandler,
+    DispatcherHandlerStop,
+    CallbackQueryHandler,
     run_async,
+    Filters,
 )
-from telegram.utils.helpers import escape_markdown, mention_html
+from telegram.utils.helpers import mention_html, escape_markdown
 
-from Himawari import DRAGONS, LOGGER, dispatcher
-from Himawari.modules.connection import connected
-from Himawari.modules.disable import DisableAbleCommandHandler
-from Himawari.modules.helper_funcs.alternate import send_message, typing_action
-from Himawari.modules.helper_funcs.chat_status import user_admin
-from Himawari.modules.helper_funcs.extraction import extract_text
-from Himawari.modules.helper_funcs.filters import CustomFilters
-from Himawari.modules.helper_funcs.handlers import MessageHandlerChecker
-from Himawari.modules.helper_funcs.misc import build_keyboard_parser
-from Himawari.modules.helper_funcs.msg_types import get_filter_type
-from Himawari.modules.helper_funcs.string_handling import (
+from ShinobuRobot import dispatcher, LOGGER, DRAGONS
+from ShinobuRobot.modules.disable import DisableAbleCommandHandler
+from ShinobuRobot.modules.helper_funcs.handlers import MessageHandlerChecker
+from ShinobuRobot.modules.helper_funcs.chat_status import user_admin
+from ShinobuRobot.modules.helper_funcs.extraction import extract_text
+from ShinobuRobot.modules.helper_funcs.filters import CustomFilters
+from ShinobuRobot.modules.helper_funcs.misc import build_keyboard_parser
+from ShinobuRobot.modules.helper_funcs.msg_types import get_filter_type
+from ShinobuRobot.modules.helper_funcs.string_handling import (
+    split_quotes,
     button_markdown_parser,
     escape_invalid_curly_brackets,
     markdown_to_html,
-    split_quotes,
 )
-from Himawari.modules.sql import cust_filters_sql as sql
+from ShinobuRobot.modules.sql import cust_filters_sql as sql
+
+from ShinobuRobot.modules.connection import connected
+
+from ShinobuRobot.modules.helper_funcs.alternate import send_message, typing_action
 
 HANDLER_GROUP = 10
 
@@ -48,7 +50,6 @@ ENUM_FUNC_MAP = {
 }
 
 
-@run_async
 @typing_action
 def list_handlers(update, context):
     chat = update.effective_chat
@@ -267,7 +268,6 @@ def stop_filter(update, context):
     )
 
 
-@run_async
 def reply_filter(update, context):
     chat = update.effective_chat  # type: Optional[Chat]
     message = update.effective_message  # type: Optional[Message]
@@ -403,21 +403,23 @@ def reply_filter(update, context):
                                 LOGGER.exception(
                                     "Failed to send message: " + excp.message
                                 )
+                                pass
                 else:
-                    try:
+                    if ENUM_FUNC_MAP[filt.file_type] == dispatcher.bot.send_sticker:
+                        ENUM_FUNC_MAP[filt.file_type](
+                            chat.id,
+                            filt.file_id,
+                            reply_to_message_id=message.message_id,
+                            reply_markup=keyboard,
+                        )
+                    else:
                         ENUM_FUNC_MAP[filt.file_type](
                             chat.id,
                             filt.file_id,
                             caption=markdown_to_html(filtext),
                             reply_to_message_id=message.message_id,
                             parse_mode=ParseMode.HTML,
-                            disable_web_page_preview=True,
                             reply_markup=keyboard,
-                        )
-                    except BadRequest:
-                        send_message(
-                            message,
-                            "I don't have the permission to send the content of the filter.",
                         )
                 break
             else:
@@ -457,6 +459,7 @@ def reply_filter(update, context):
                                 )
                             except BadRequest as excp:
                                 LOGGER.exception("Error in filters: " + excp.message)
+                                pass
                         elif excp.message == "Reply message not found":
                             try:
                                 context.bot.send_message(
@@ -468,6 +471,7 @@ def reply_filter(update, context):
                                 )
                             except BadRequest as excp:
                                 LOGGER.exception("Error in filters: " + excp.message)
+                                pass
                         else:
                             try:
                                 send_message(
@@ -476,6 +480,7 @@ def reply_filter(update, context):
                                 )
                             except BadRequest as excp:
                                 LOGGER.exception("Error in filters: " + excp.message)
+                                pass
                             LOGGER.warning(
                                 "Message %s could not be parsed", str(filt.reply)
                             )
@@ -491,10 +496,10 @@ def reply_filter(update, context):
                         send_message(update.effective_message, filt.reply)
                     except BadRequest as excp:
                         LOGGER.exception("Error in filters: " + excp.message)
+                        pass
                 break
 
 
-@run_async
 def rmall_filters(update, context):
     chat = update.effective_chat
     user = update.effective_user
@@ -521,7 +526,6 @@ def rmall_filters(update, context):
         )
 
 
-@run_async
 def rmall_callback(update, context):
     query = update.callback_query
     chat = update.effective_chat
@@ -607,10 +611,10 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
- ❍ /filters*:* List all active filters saved in the chat.
+ • `/filters`*:* List all active filters saved in the chat.
 
 *Admin only:*
- ❍ /filter <keyword> <reply message>*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
+ • `/filter <keyword> <reply message>`*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
 is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
 keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
 doin?
@@ -622,13 +626,13 @@ doin?
  Reply 2
  %%%
  Reply 3`
- ❍ /stop <filter keyword>*:* Stop that filter.
+ • `/stop <filter keyword>`*:* Stop that filter.
 
 *Chat creator only:*
- ❍ /removeallfilters*:* Remove all chat filters at once.
+ • `/removeallfilters`*:* Remove all chat filters at once.
 
 *Note*: Filters also support markdown formatters like: {first}, {last} etc.. and buttons.
-Check ❍ /markdownhelp to know more!
+Check `/markdownhelp` to know more!
 
 """
 
@@ -637,12 +641,18 @@ __mod_name__ = "Filters"
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
 RMALLFILTER_HANDLER = CommandHandler(
-    "removeallfilters", rmall_filters, filters=Filters.group
+    "removeallfilters", rmall_filters, filters=Filters.chat_type.groups, run_async=True
 )
-RMALLFILTER_CALLBACK = CallbackQueryHandler(rmall_callback, pattern=r"filters_.*")
-LIST_HANDLER = DisableAbleCommandHandler("filters", list_handlers, admin_ok=True)
+RMALLFILTER_CALLBACK = CallbackQueryHandler(
+    rmall_callback, pattern=r"filters_.*", run_async=True
+)
+LIST_HANDLER = DisableAbleCommandHandler(
+    "filters", list_handlers, admin_ok=True, run_async=True
+)
 CUST_FILTER_HANDLER = MessageHandler(
-    CustomFilters.has_text & ~Filters.update.edited_message, reply_filter
+    CustomFilters.has_text & ~Filters.update.edited_message,
+    reply_filter,
+    run_async=True,
 )
 
 dispatcher.add_handler(FILTER_HANDLER)
@@ -658,11 +668,6 @@ __handlers__ = [
     LIST_HANDLER,
     (CUST_FILTER_HANDLER, HANDLER_GROUP, RMALLFILTER_HANDLER),
 ]
-                            
-       
-
-
-
 
 
 
