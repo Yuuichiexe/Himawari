af33ad9bfd5a3ad73b1729bf4b5739e43cf359ee
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2023-06-25 10:38:11 +0530

    Create good morning.py
    Signed-off-by: Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>

---
+++ Himawari/modules/good morning.py
@@ -0,0 +1,17 @@
+from telethon import Button
+
+from Himawari import tbot
+from Himawari.events import register
+
+PHOTO = "https://graph.org/file/49ccd6c3fd1ab3a8d2701.mp4"
+
+
+@register(pattern=("Good morning"))
+async def awake(event):
+    NEKO = f"Wᴇʟᴄᴏᴍᴇ 🥀  ᴛʜɪꜱ ʙᴇᴀᴜᴛɪꜰᴜʟ ᴍᴏʀɴɪɴɢ ᴡɪᴛʜ ᴀ ꜱᴍɪʟᴇ ᴏɴ ʏᴏᴜʀ ꜰᴀᴄᴇ. ɪ ʜᴏᴘᴇ ʏᴏᴜ'ʟʟ ʜᴀᴠᴇ ᴀ ɢʀᴇᴀᴛ ᴅᴀʏ ᴛᴏᴅᴀʏ. ᴡɪꜱʜɪɴɢ ʏᴏᴜ ᴀ ᴠᴇʀʏ ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ! {event.sender.first_name}"
+    BUTTON = [
+        [
+            Button.url("Mᴇᴇᴛ ᴍᴇ Hᴇʀᴇ ✨", "https://telegram.dog/Mega_Bots_Support"),
+        ]
+    ]
+    await tbot.send_file(event.chat_id, PHOTO, caption=NEKO, buttons=BUTTON)
