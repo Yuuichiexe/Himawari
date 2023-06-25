6309deade6ade3ece8c315eb2e21ce53d8bdd2b0
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2023-06-25 09:51:22 +0530

    Update stickers.py
    Signed-off-by: Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>

--- Himawari/modules/stickers.py
+++ Himawari/modules/stickers.py
@@ -43,7 +43,9 @@ def cb_sticker(update: Update, context: CallbackContext):
     if len(split) == 1:
         msg.reply_text("Provide some name to search for pack.")
         return
-    text = requests.get(combot_stickers_url + split[1]).text
+
+    scraper = cloudscraper.create_scraper()
+    text = scraper.get(combot_stickers_url + split[1]).text
     soup = bs(text, "lxml")
     results = soup.find_all("a", {"class": "sticker-pack__btn"})
     titles = soup.find_all("div", "sticker-pack__title")
