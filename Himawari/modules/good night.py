f33094c20d700287ada7a75c58cb230af12748a2
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2023-06-25 10:43:08 +0530

    Create good night.py
    Signed-off-by: Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>

---
+++ Himawari/modules/good night.py
@@ -0,0 +1,17 @@
+from telethon import Button
+
+from Himawari import tbot
+from Himawari.events import register
+
+PHOTO = "https://graph.org/file/c8800bdf565ea841d87ee.mp4"
+
+
+@register(pattern=("Good night"))
+async def awake(event):
+    NEKO = f"Gᴏᴏᴅ  ɴɪɢʜᴛ 🥀 ɪ ʜᴏᴘᴇ  ᴛᴏᴍᴏʀʀᴏᴡ ɪꜱ ᴛʜᴇ ʙᴇꜱᴛ  ᴅᴀʏ ɪɴ ʏᴏᴜʀ ʟɪꜰᴇ. {event.sender.first_name}"
+    BUTTON = [
+        [
+            Button.url("Mᴇᴇᴛ ᴍᴇ Hᴇʀᴇ ✨", "https://telegram.dog/xtromxsupport"),
+        ]
+    ]
+    await tbot.send_file(event.chat_id, PHOTO, caption=NEKO, buttons=BUTTON)
