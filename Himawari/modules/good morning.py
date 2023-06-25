364776f2db4f0df53981f73c30f4877f870099dc
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2023-06-25 14:27:09 +0530

    Update good morning.py
    Signed-off-by: Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>

--- Himawari/modules/good morning.py
+++ Himawari/modules/good morning.py
@@ -8,10 +8,10 @@ PHOTO = "https://graph.org/file/49ccd6c3fd1ab3a8d2701.mp4"
 
 @register(pattern=("Good morning"))
 async def awake(event):
-    NEKO = f"Wᴇʟᴄᴏᴍᴇ 🥀  ᴛʜɪꜱ ʙᴇᴀᴜᴛɪꜰᴜʟ ᴍᴏʀɴɪɴɢ ᴡɪᴛʜ ᴀ ꜱᴍɪʟᴇ ᴏɴ ʏᴏᴜʀ ꜰᴀᴄᴇ. ɪ ʜᴏᴘᴇ ʏᴏᴜ'ʟʟ ʜᴀᴠᴇ ᴀ ɢʀᴇᴀᴛ ᴅᴀʏ ᴛᴏᴅᴀʏ. ᴡɪꜱʜɪɴɢ ʏᴏᴜ ᴀ ᴠᴇʀʏ ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ! {event.sender.first_name}"
+    MEOW = f"Wᴇʟᴄᴏᴍᴇ 🥀  ᴛʜɪꜱ ʙᴇᴀᴜᴛɪꜰᴜʟ ᴍᴏʀɴɪɴɢ ᴡɪᴛʜ ᴀ ꜱᴍɪʟᴇ ᴏɴ ʏᴏᴜʀ ꜰᴀᴄᴇ. ɪ ʜᴏᴘᴇ ʏᴏᴜ'ʟʟ ʜᴀᴠᴇ ᴀ ɢʀᴇᴀᴛ ᴅᴀʏ ᴛᴏᴅᴀʏ. ᴡɪꜱʜɪɴɢ ʏᴏᴜ ᴀ ᴠᴇʀʏ ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ! {event.sender.first_name}"
     BUTTON = [
         [
             Button.url("Mᴇᴇᴛ ᴍᴇ Hᴇʀᴇ ✨", "https://telegram.dog/Mega_Bots_Support"),
         ]
     ]
-    await tbot.send_file(event.chat_id, PHOTO, caption=NEKO, buttons=BUTTON)
+    await tbot.send_file(event.chat_id, PHOTO, caption=MEOW, buttons=BUTTON)
