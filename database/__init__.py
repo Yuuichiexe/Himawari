138acefdc0b05db1950c812ef5a53a86eaf15563
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2023-06-26 16:11:11 +0530

    Update __init__.py
    Signed-off-by: Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>

--- database/__init__.py
+++ database/__init__.py
@@ -6,7 +6,7 @@
 """
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
-from Himawari.config import DB_URL
+from Himawari.config import DATABASE_URI
 
 mongo = MongoClient(DATABASE_URI)
 dbname = mongo.MissKatyDB
