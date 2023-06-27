d0aff1a9b76a94cac94d8b73ad2616fa878b6078
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2023-06-27 17:09:13 +0530

    Update approval.py
    Signed-off-by: Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>

--- Himawari/modules/approval.py
+++ Himawari/modules/approval.py
@@ -233,6 +233,6 @@ dispatcher.add_handler(APPROVAL)
 dispatcher.add_handler(UNAPPROVEALL)
 dispatcher.add_handler(UNAPPROVEALL_BTN)
 
-__mod_name__ = "Approval"
+__mod_name__ = "𝙰ᴘᴘʀᴏᴠᴀʟ"
 __command_list__ = ["approve", "unapprove", "approved", "approval"]
 __handlers__ = [APPROVE, DISAPPROVE, APPROVED, APPROVAL]
