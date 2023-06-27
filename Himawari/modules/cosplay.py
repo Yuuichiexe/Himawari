a233cfc43b200becb39930f8003939f979916580
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2023-06-27 17:48:06 +0530

    Update cosplay.py
    Signed-off-by: Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>

--- Himawari/modules/cosplay.py
+++ Himawari/modules/cosplay.py
@@ -36,7 +36,7 @@ async def waifu(event):
   r = requests.get("https://waifu-api.vercel.app/items/1").json()
   await event.reply(file=r)
 
-__mod_name__ = "Cosplay"
+__mod_name__ = "𝙲ᴏꜱᴘʟᴀʏ"
 __help__ = """
 Just a weeb type module to get anime cosplay and lewd pictures
 - /cosplay
