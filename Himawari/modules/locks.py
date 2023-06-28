d3c002251b6ce243bccf1b228322b7976d2fb0fa
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2023-06-28 13:25:07 +0530

    Update locks.py
    Signed-off-by: Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>

--- Himawari/modules/locks.py
+++ Himawari/modules/locks.py
@@ -606,7 +606,7 @@ Locking bots will stop non-admins from adding bots to the chat.
  ➩ Unlocking permission *pin* will allow members (non-admins) to pinned a message in a group
 """
 
-__mod_name__ = "Locks"
+__mod_name__ = "𝙻ᴏᴄᴋꜱ"
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes, run_async=True)
 LOCK_HANDLER = CommandHandler(
