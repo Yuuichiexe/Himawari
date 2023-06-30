9b4ecce95c35a73ca41b1591cf9c15e6a968a1c6
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2023-06-30 20:19:30 +0530

    Update alive.py
    Signed-off-by: Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>

--- Himawari/modules/alive.py
+++ Himawari/modules/alive.py
@@ -1,38 +1,57 @@
-"""
-MIT License
+import asyncio
+import random
+from sys import version_info
 
-Copyright (c) 2022 Arsh
+from pyrogram import __version__ as pver
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from telegram import __version__ as lver
+from telethon import __version__ as tver
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+from GENOs.helper import PHOTO
+from Himawari import BOT_NAME
+from Himawari import BOT_USERNAME as fuck
+from Himawari import OWNER_USERNAME, SUPPORT_CHAT, UPDATES_CHANNEL, pgram
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+ASAU = [
+    [
+        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇꜱ", url=f"https://t.me/{UPDATES_CHANNEL}"),
+        InlineKeyboardButton(text="ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{SUPPORT_CHAT}"),
+    ],
+    [
+        InlineKeyboardButton(
+            text="ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
+            url=f"https://t.me/{fuck}?startgroup=true",
+        ),
+    ],
+]
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 
-from telethon import events, Button, custom
-from Himawari.events import register
-from Himawari import telethn as tbot, SUPPORT_CHAT, UPDATES_CHANNEL, BOT_NAME
-HIMAWARI = "https://telegra.ph/file/d72cf3adb5a740495809a.jpg"
-@register(pattern=("/alive"))
-async def awake(event):
-  STB = event.sender.first_name
-  STB = f"**I'ᴍ {BOT_NAME}** \n\n" + "**I'ᴍ ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ**\n\n"
-  STB += "**Mʏ ᴏᴡɴᴇʀ @NIOAX **\n\n"
-  STB += "**Pʏᴛʜᴏɴ-ᴛᴇʟᴇɢʀᴀᴍ-ʙᴏᴛ : 13.12**\n\n"
-  BUTTON = [[Button.url("Sᴜᴘᴘᴏʀᴛ", "https://t.me/Mega_Bots_Support"), Button.url("Uᴘᴅᴀᴛᴇꜱ", "https://t.me/Mega_Bot_Updates")]]
-  await tbot.send_file(event.chat_id, HIMAWARI, caption=STB,  buttons=BUTTON)
-
-  # thanks to stb the gay
+@pgram.on_message(filters.command("alive"))
+async def restart(client, m: Message):
+    await m.delete()
+    accha = await m.reply("⚡")
+    await asyncio.sleep(1)
+    await accha.edit("ᴀʟɪᴠɪɴɢ..")
+    await asyncio.sleep(0.1)
+    await accha.edit("ᴀʟɪᴠɪɴɢ ʙᴀʙʏ ....")
+    await accha.delete()
+    await asyncio.sleep(0.1)
+    umm = await m.reply_sticker(
+        "CAACAgUAAxkBAANTZJxstC8mgokKJU1swj-pvymxU04AAk8KAAKtAAHoVIp9NL27wY4oLwQ"
+    )
+    await asyncio.sleep(0.1)
+    await m.reply_photo(
+        random.choice(PHOTO),
+        caption=f"""**ʜᴇʏ, ɪ ᴀᴍ {BOT_NAME}**
+     ▱▱▱▱▱▱▱▱▱▱▱▱
+» **ᴍʏ ᴏᴡɴᴇʀ :** [ɴɪᴏᴀx](https://t.me/NIOAX)
+» **ᴍʏ ᴅᴇᴠ :** [ɢᴇɴᴏꜱ](https://t.me/Fuckingenos)
+» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{lver}`
+» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tver}`
+» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pver}`
+» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{version_info[0]}.{version_info[1]}.{version_info[2]}`
+⍟ **ʙᴏᴛ ᴠᴇʀꜱɪᴏɴ :** `1.0`
+     ▱▱▱▱▱▱▱▱▱▱▱▱""",
+        reply_markup=InlineKeyboardMarkup(ASAU),
+    )
